e1c7a576c594ccc4045dc2e685272e852c3e0a81
CSLprojectOfficial <46883571+cslsproject@users.noreply.github.com>
2019-05-22 23:18:19 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i=1;
+    char *command = argv[1];
+    char *filename = argv[2];
+    if (strcmp(command, "install") == 0)
+    {
+      
+    }
+    return 0;
+}
