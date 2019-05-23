86dd167e4c603c1137dc89586efa7a8a9d14441c
CSLprojectOfficial <46883571+cslsproject@users.noreply.github.com>
2019-05-24 03:03:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -11,3 +11,27 @@ int main(int argc, char **argv)
     }
     return 0;
 }
+
+
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(int argc, char **argv)
+{
+    int i=1;
+    char *command = argv[1];
+    char *filename = argv[2];
+    if (strcmp(command, "install") == 0)
+    {
+      printf("%s 확인\n", command);  //debug 1 command
+      printf("%s 확인\n", filename);  //debug 2 filename
+      int main()
+      {
+        int config/pud/filename.taz.gz=2;
+        
+        
+        
+      }
+    }
+    return 0;
+}
